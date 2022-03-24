871ae3cb7a55857e77e036eb9d6c7a1fa8170009
Psami-wondah <okechukwusamuel16@gmail.com>
2022-03-24 12:00:28 +0100

    cors

--- api.py
+++ api.py
@@ -42,7 +42,12 @@ app = FastAPI(
 )
 
 
-origins = ["*"]
+origins = [
+    "https://localhost:3000",
+    "http://localhost:3000",
+    "https://localhost:3000/",
+    "https://localhost:3000/",
+]
 
 
 app.include_router(user)
@@ -61,31 +66,13 @@ async def startup():
     User.init()
 
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-ALLOWED_ORIGINS = "*"  # or 'foo.com', etc.
-
-# handle CORS preflight requests
-@app.options("/{rest_of_path:path}")
-async def preflight_handler(request: Request, rest_of_path: str) -> Response:
-    response = Response()
-    response.headers["Access-Control-Allow-Origin"] = ALLOWED_ORIGINS
-    response.headers["Access-Control-Allow-Methods"] = "POST, GET, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
-    return response
+# or 'foo.com', etc.
 
 
-# set CORS headers
-@app.middleware("http")
-async def add_CORS_header(request: Request, call_next):
-    response = await call_next(request)
-    response.headers["Access-Control-Allow-Origin"] = ALLOWED_ORIGINS
-    response.headers["Access-Control-Allow-Methods"] = "POST, GET, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
-    return response
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
